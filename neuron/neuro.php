aefcc9bcc4087c71d582561971c7b80448223112
Leo <33494928+sl3w@users.noreply.github.com>
2020-09-12 03:51:50 +0400

    code clearing

--- neuron/neuro.php
+++ neuron/neuro.php
@@ -10,11 +10,10 @@ define("TEACH_KOEF2", 0.65);
 global $isTeach;
 $isTeach = false;
 
-global $answers, $ethalons;
+global $answers, $ethalons, $fs, $numPrimer, $yOuts;
 $answers = array();
 $ethalons = array();
 
-//$x = array(1, 0);
 
 global $dopWeight;
 
@@ -51,10 +50,6 @@ $weights = array(
     "w312" => 0.39,
 );
 
-//$weights = array();
-//ksort($weights);
-//pre($weights);
-
 if ($_POST['do'] == 'init') {
     $isTeach = false;
 
@@ -71,50 +66,63 @@ if ($_POST['do'] == 'init') {
 //    startEpoch($weights, array(3, 1, 2), array(1));
 
 
+    $numPrimer = 1;
     startEpoch($weights, array(0, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(2, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(3, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 0, 1), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 0, 2), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 0, 3), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 1, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 1, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(2, 1, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(3, 1, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(2, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(3, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(1, 0, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 2, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 3, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 0), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 2), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 3), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 1), array(1));
 
 
-//    pre($answers);
-//    pre($ethalons);
-
     $er = calcError($answers, $ethalons);
-
-//    pre($er);
-
 
     $res['error'] = $er;
     $res['weights'] = $weights;
+    $res['funcAct'] = $fs;
+//    $res['answers'] = $yOuts;
     echo json_encode($res);
+
 } elseif ($_POST['do'] == 'teach') {
     $isTeach = true;
 
     $weights = $_POST['weights'];
+    $fs = $_POST['funcAct'];
+//    $yOuts = $_POST['answers'];
     $countProhodov = $_POST['countProhodov'];
-
-
-//    echo json_encode($weights);
-//    return;
-
-//pre($weights);
 
     for ($i = 0; $i < $countProhodov; $i++) {
 //        startEpoch($weights, array(2, 1, 3), array(1));
@@ -130,28 +138,47 @@ if ($_POST['do'] == 'init') {
 //        startEpoch($weights, array(3, 1, 2), array(1));
 
 
-        startEpoch($weights, array(0, 0, 0), array(0));
-        startEpoch($weights, array(2, 0, 0), array(0));
-        startEpoch($weights, array(3, 0, 0), array(0));
-        startEpoch($weights, array(0, 0, 1), array(0));
-        startEpoch($weights, array(0, 0, 2), array(0));
-        startEpoch($weights, array(0, 0, 3), array(0));
-        startEpoch($weights, array(0, 1, 0), array(0));
-        startEpoch($weights, array(0, 1, 1), array(1));
-        startEpoch($weights, array(2, 1, 1), array(1));
-        startEpoch($weights, array(3, 1, 1), array(1));
-        startEpoch($weights, array(1, 0, 0), array(0));
-        startEpoch($weights, array(2, 0, 0), array(0));
-        startEpoch($weights, array(3, 0, 0), array(0));
-        startEpoch($weights, array(1, 0, 1), array(1));
-        startEpoch($weights, array(1, 2, 1), array(1));
-        startEpoch($weights, array(1, 3, 1), array(1));
-        startEpoch($weights, array(1, 1, 0), array(1));
-        startEpoch($weights, array(1, 1, 2), array(1));
-        startEpoch($weights, array(1, 1, 3), array(1));
-        startEpoch($weights, array(1, 1, 1), array(1));
+        $numPrimer = 1;
+        startEpoch($weights, array(0, 0, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(2, 0, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(3, 0, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(0, 0, 1), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(0, 0, 2), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(0, 0, 3), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(0, 1, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(0, 1, 1), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(2, 1, 1), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(3, 1, 1), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 0, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(2, 0, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(3, 0, 0), array(0), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 0, 1), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 2, 1), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 3, 1), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 1, 0), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 1, 2), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 1, 3), array(1), true);
+        $numPrimer++;
+        startEpoch($weights, array(1, 1, 1), array(1), true);
     }
-//pre($weights, 1);
 
     $isTeach = false;
 
@@ -171,41 +198,53 @@ if ($_POST['do'] == 'init') {
 //    startEpoch($weights, array(3, 2, 1), array(1));
 //    startEpoch($weights, array(3, 1, 2), array(1));
 
+    $numPrimer = 1;
     startEpoch($weights, array(0, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(2, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(3, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 0, 1), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 0, 2), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 0, 3), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 1, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(0, 1, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(2, 1, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(3, 1, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(2, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(3, 0, 0), array(0));
+    $numPrimer++;
     startEpoch($weights, array(1, 0, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 2, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 3, 1), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 0), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 2), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 3), array(1));
+    $numPrimer++;
     startEpoch($weights, array(1, 1, 1), array(1));
 
-
-//    pre($answers);
-//    pre($ethalons);
 
     $er = calcError($answers, $ethalons);
 
     $res['error'] = $er;
     $res['weights'] = $weights;
     echo json_encode($res);
-
-
-//$isTeach = false;
-//pre(startEpoch($weights, array(0, 0, 1)));
-
 
 } elseif ($_POST['do'] == 'test') {
     $weights = $_POST['weights'];
@@ -221,17 +260,12 @@ if ($_POST['do'] == 'init') {
     echo json_encode($res);
 }
 
-
-function startEpoch(&$weights, $x, $eth = false)
+function startEpoch(&$weights, $x, $eth = false, $skipForward = false)
 {
-    global $isTeach;
-//    $bigSs = array();
-    $fs = array();
-    generateX($x, $fs);
-    $yOut = firstForward($weights, $x, $fs/*, $bigSs*/);
+    global $isTeach, $fs, $answers, $ethalons;
+    generateX($x);
+    $yOut = firstForward($weights, $x /*, $bigSs*/);
 
-    global $answers, $ethalons;
-//    $answers[] = number_format($sOut, 9);
     $answers[] = $yOut;
     $ethalons[] = $eth;
 
@@ -239,69 +273,36 @@ function startEpoch(&$weights, $x, $eth = false)
         return $yOut;
     }
 
-//    pre($sOut, 1);
-
-//    ksort($weights);
-//    pre($weights);
-
     foreach ($yOut as $k => $value) {
         $proiz = calcProizvod($value);
 
-//        pre($value);
-
-//        pre($p, 1);
-
-//        $y["y" . $k] = $p;
-
-//        if ($isTeach) {
-//            $fs["f" . (COUNT_SLOY + 1) . $k] = $proiz;
-//            pre($eth[$k - 1]);
-//            pre($p);
-
         $d = $value - $eth[$k];
 
-//            pre($d, 1);
-
         $ds["d" . (COUNT_SLOY + 1) . ($k + 1)] = $d * $proiz;
-//            pre($ds["d" . (COUNT_SLOY + 1) . ($k)]);
-//        }
     }
 
-//    pre($ds, 1);
-
-//    pre($fs, 1);
 
     if ($isTeach) {
-        calcErrors($ds, $weights, $fs);
-//
-//        pre($weights);
-        $newWeights = weightCorrection($weights, $x, $fs, $ds);
-//
-//        ksort($newWeights);
-////        pre($weights);
-        $weights = $newWeights;
-//        unset($newWeights);
-//        pre($weights);
-////        pre("--------------------------------");
-//        return $y;
-    }
-//    else
-//        return $y;
+        calcErrors($ds, $weights);
 
-//    pre($ds, 1);
+        $newWeights = weightCorrection($weights, $x, $ds);
+
+        $weights = $newWeights;
+    }
 }
 
-function generateX(&$x, &$fs)
+function generateX($x)
 {
     for ($i = 0; $i < count($x); $i++) {
-        $fs["f0" . ($i + 1)] = $x[$i];
+        setValueFuncAct(0, $i + 1, $x[$i]);
     }
 }
 
-function firstForward(&$weights, $x, &$fs, &$bigSs = array())
+function firstForward(&$weights, $x)
 {
-    for ($i = 1; $i <= COUNT_SLOY; $i++) {
-        for ($j = 1; $j <= COUNT_ON_SLOY; $j++) {
+    for ($i = 1; $i <= COUNT_SLOY + 1; $i++) {
+        $jMax = $i == COUNT_SLOY + 1 ? COUNT_OUTPUTS : COUNT_ON_SLOY;
+        for ($j = 1; $j <= $jMax; $j++) {
 
             if ($i == 1)
                 $kMax = count($x);
@@ -315,53 +316,39 @@ function firstForward(&$weights, $x, &$fs, &$bigSs = array())
                 if (!key_exists($weightKey, $weights)) {
                     $weights[$weightKey] = randWeight();
                 }
-//                if($i != 1) {
-//                    pre("f" . ($i - 1) . $k . ": " . $fs["f" . ($i - 1) . $k]);
-//                    pre($weightKey . ": " . $weights[$weightKey]);
-//                    pre("____________________________________");
-
-//                }
-                $S += $fs["f" . ($i - 1) . $k] * $weights[$weightKey];
+                $S += getValueFuncAct($i - 1, $k) * $weights[$weightKey];
 //                $S += 1 * getRandDopWeight($weightKey);
             }
 
-            //die();
+//            $bigSs["s" . $i . $j] = $S;
 
-            $bigSs["s" . $i . $j] = $S;
+            setValueFuncAct($i, $j, calcFa($S));
 
-            $fs["f" . $i . $j] = calcFa($S);
+            if ($i == COUNT_SLOY + 1)
+                $y[] = calcFa($S);
         }
-        //die();
-
     }
 
-//    pre($bigSs);
-//    pre($fs, 1);
 
-    for ($j = 1; $j <= COUNT_OUTPUTS; $j++) {
-        $S = 0;
-        for ($i = 1; $i <= COUNT_ON_SLOY; $i++) {
-            $weightKey = "w" . (COUNT_SLOY + 1) . $j . $i;
-            if (!key_exists($weightKey, $weights)) {
-                $weights[$weightKey] = randWeight();
-            }
-
-//            pre("f" . COUNT_SLOY . $i . ": " . $fs["f" . COUNT_SLOY . $i]);
-//            pre($weightKey . ": " . $weights[$weightKey]);
-//            pre("____________________________________");
-
-            $S += $weights[$weightKey] * $fs["f" . COUNT_SLOY . $i];
-//            $S += 1 * getRandDopWeight($weightKey);
-        }
-
-        $bigSs["s" . $i . $j] = $S;
-
-        $funAct = calcFa($S);
-
-        $fs["f" . (COUNT_SLOY + 1) . $j] = $funAct;
-//        $y[$j] = $funAct;
-        $y[] = $funAct;
-    }
+//    for ($j = 1; $j <= COUNT_OUTPUTS; $j++) {
+//        $S = 0;
+//        for ($i = 1; $i <= COUNT_ON_SLOY; $i++) {
+//            $weightKey = "w" . (COUNT_SLOY + 1) . $j . $i;
+//            if (!key_exists($weightKey, $weights)) {
+//                $weights[$weightKey] = randWeight();
+//            }
+//
+//            $S += $weights[$weightKey] * getValueFuncAct(COUNT_SLOY, $i);
+////            $S += 1 * getRandDopWeight($weightKey);
+//        }
+//
+//        $bigSs["s" . $i . $j] = $S;
+//
+//        $funAct = calcFa($S);
+//
+//        setValueFuncAct(COUNT_SLOY + 1, $j, $funAct);
+//        $y[] = $funAct;
+//    }
 
     return $y;
 }
@@ -376,73 +363,46 @@ function getRandDopWeight($k)
     return $dopWeight["d" . $k];
 }
 
-function calcErrors(&$ds, $weights, $fs)
+function calcErrors(&$ds, $weights)
 {
-//    pre($ds);
-    for ($i = 1; $i <= COUNT_ON_SLOY; $i++) {
-        $curD = 0;
-        $fPr = calcProizvod($fs["f" . (COUNT_SLOY) . $i]);
-//        pre("f" . (COUNT_SLOY) . $i . ":" . $fs["f" . (COUNT_SLOY) . $i]);
-        for ($j = 1; $j <= COUNT_OUTPUTS; $j++) {
-//            for ($k = 1; $k <= COUNT_OUTPUTS; $k++) {
-            $d = $ds["d" . (COUNT_SLOY + 1) . $j];
-            $w = $weights["w" . (COUNT_SLOY + 1) . $j . $i];
+//    for ($i = 1; $i <= COUNT_ON_SLOY; $i++) {
+//        $curD = 0;
+//        $fPr = calcProizvod(getValueFuncAct(COUNT_SLOY, $i));
+//        for ($j = 1; $j <= COUNT_OUTPUTS; $j++) {
+//            $d = $ds["d" . (COUNT_SLOY + 1) . $j];
+//            $w = $weights["w" . (COUNT_SLOY + 1) . $j . $i];
+//
+//            $curD += $d * $w * $fPr;
+//            $ds["d" . COUNT_SLOY . $i] = $curD;
+//        }
+//    }
 
-//                pre("d" . (COUNT_SLOY + 1) . $j . ":" . $d);
-//                pre("w" . (COUNT_SLOY + 1) . $j . $i . ":" . $w);
-//                pre("f" . (COUNT_SLOY) . $i . ":" . $fs["f" . (COUNT_SLOY) . $i]);
-//                pre("________________");
-
-            $curD += $d * $w * $fPr;
-//            }
-            $ds["d" . COUNT_SLOY . $i] = $curD;
-        }
-    }
-//    pre($ds);
-
-    for ($i = COUNT_SLOY - 1; $i > 0; $i--) {
+    for ($i = COUNT_SLOY; $i > 0; $i--) {
         for ($j = 1; $j <= COUNT_ON_SLOY; $j++) {
+            $kMax = $i == COUNT_SLOY ? COUNT_OUTPUTS : COUNT_ON_SLOY;
+
             $curD = 0;
-
-
-//            $w = $weights["w" . $k . $i . $j];
-//            pre("w" . (COUNT_SLOY + 1) . $j . $i . ":" . $w);
-
-
-//            pre($weights);
-            for ($k = 1; $k <= COUNT_ON_SLOY; $k++) {
-
+            for ($k = 1; $k <= $kMax; $k++) {
                 $d = $ds["d" . ($i + 1) . $k];
-//                pre("d" . ($i + 1) . $k . ":" . $d);
-
                 $w = $weights["w" . ($i + 1) . $k . $j];
-//                pre("w" . ($i + 1) . $k . $j . ":" . $w);
-
 
                 $curD += $d * $w;
             }
-//            pre($fs);
 
-//            pre("f". $i. $j . ":" . $fs["f". $i. $j]);
-//            pre($curD);
-            $curD *= calcProizvod($fs["f" . $i . $j]);
-//            die();
+            $curD *= calcProizvod(getValueFuncAct($i, $j));
             $ds["d" . $i . $j] = $curD;
         }
-//        pre($ds);
     }
-//    pre($ds);
 }
 
-function weightCorrection($weights, $x, &$fs, $ds)
+function weightCorrection($weights, $x, $ds)
 {
     $newWeights = array();
 
-//    pre($ds);
-    for ($i = 1; $i <= COUNT_SLOY; $i++) {
-        for ($j = 1; $j <= COUNT_ON_SLOY; $j++) {
+    for ($i = 1; $i <= COUNT_SLOY + 1; $i++) {
+        $jMax = $i == COUNT_SLOY + 1 ? COUNT_OUTPUTS : COUNT_ON_SLOY;
+        for ($j = 1; $j <= $jMax; $j++) {
 
-//            pre($ds["d" . $i . $j]);
             $multDsA = $ds["d" . $i . $j] * TEACH_KOEF2 * (-1);
 
             if ($i == 1)
@@ -453,37 +413,24 @@ function weightCorrection($weights, $x, &$fs, $ds)
 
             for ($k = 1; $k <= $kMax; $k++) {
 
-//                pre($fs["f" . ($i - 1) . $k]);
-                $newW = $fs["f" . ($i - 1) . $k] * $multDsA;
+                $newW = getValueFuncAct($i - 1, $k) * $multDsA;
 
                 $w = $weights["w" . $i . $j . $k];
-//                pre($w);
                 $newWeights["w" . $i . $j . $k] = $w + $newW;
             }
-
-//            die();
-        }
-
-    }
-//pre($weights);
-//pre($newWeights);
-
-    for ($j = 1; $j <= COUNT_OUTPUTS; $j++) {
-//        $fPr = calcProizvod($fs["f" . (COUNT_SLOY + 1) . $j]);
-//        $multDsFPrA = $ds["d" . (COUNT_SLOY + 1) . $j] * $fPr * TEACH_KOEF;
-
-//        pre($ds["d" . (COUNT_SLOY + 1) . $j]);
-        $multDsA = $ds["d" . (COUNT_SLOY + 1) . $j] * TEACH_KOEF2 * (-1);
-
-        for ($i = 1; $i <= COUNT_ON_SLOY; $i++) {
-//            pre($fs["f" . COUNT_SLOY . $i]);
-            $newW = $fs["f" . COUNT_SLOY . $i] * $multDsA;
-            $w = $weights["w" . (COUNT_SLOY + 1) . $j . $i];
-//            pre($w);
-            $newWeights["w" . (COUNT_SLOY + 1) . $j . $i] = $w + $newW;
         }
     }
-//    pre($newWeights);
+
+//    for ($j = 1; $j <= COUNT_OUTPUTS; $j++) {
+//        $multDsA = $ds["d" . (COUNT_SLOY + 1) . $j] * TEACH_KOEF2 * (-1);
+//
+//        for ($i = 1; $i <= COUNT_ON_SLOY; $i++) {
+//            $newW = getValueFuncAct(COUNT_SLOY, $i) * $multDsA;
+//            $w = $weights["w" . (COUNT_SLOY + 1) . $j . $i];
+//
+//            $newWeights["w" . (COUNT_SLOY + 1) . $j . $i] = $w + $newW;
+//        }
+//    }
     return $newWeights;
 }
 
@@ -507,17 +454,25 @@ function calcError($answers, $ethalons)
 {
     $sum = 0;
     foreach ($answers as $k => $answerVector) {
-        //pre($answerVector);
-        //pre($ethalons[$k]);
         $ethalonVector = $ethalons[$k];
         foreach ($answerVector as $l => $answer) {
             $sum += pow($answer - $ethalonVector[$l], 2);
         }
     }
-//    pre($sum, 1);
     return $sum / 2;
 }
 
+function getValueFuncAct($i, $j)
+{
+    global $fs, $numPrimer;
+    return $fs["f" . $i . $j . "-" . $numPrimer];
+}
+
+function setValueFuncAct($i, $j, $value)
+{
+    global $fs, $numPrimer;
+    $fs["f" . $i . $j . "-" . $numPrimer] = $value;
+}
 
 function pre($var, $die = false)
 {
@@ -526,4 +481,26 @@ function pre($var, $die = false)
     echo '</pre>';
     if ($die)
         die('Debug in PRE');
+}
+
+function ulogging($input, $logname = 'debug', $dt = false)
+{
+    $endLine = "\r\n"; #PHP_EOL не используется, т.к. иногда это нужно конфигурировать это
+
+    $fp = fopen('' . $logname . '.txt', "a+");
+
+    if (is_string($input)) {
+        $writeStr = $input;
+    } else {
+        $writeStr = print_r($input, true);
+    }
+
+    if ($dt) {
+        fwrite($fp, date('d.m.Y H:i:s') . $endLine);
+    }
+
+    fwrite($fp, $writeStr . $endLine);
+
+    fclose($fp);
+    return true;
 }
