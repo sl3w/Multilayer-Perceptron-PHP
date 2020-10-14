ecb4dd7a6d55b4b84f75cca7b72d62447f18ec23
Leo <33494928+sl3w@users.noreply.github.com>
2020-10-14 22:41:57 +0400

    prognosis

---
+++ neuron/prognosis/neuro.php
@@ -0,0 +1,425 @@
+<?php
+define("REAL_DATA", false);
+
+global $isTeach, $numPrimer, $teachKoef, $teachKoefStandart, $momentsKoef;
+global $hideLayCount, $hideLayNeuronCount, $outLayNeuronCount;
+global $testData, $futureTestData;
+
+$isTeach = false;
+
+global $answers, $ethalons, $funcsAct, $dopWeights, $weightsPast;
+
+global $do;
+$do = $_POST['do'];
+
+//$hideLayCount = $_POST['hideLayCount'];
+$hideLayCount = 1;
+$hideLayNeuronCount = $_POST['hideLayNeuronCount'];
+$outLayNeuronCount = 1;
+
+//$weights = array(
+//    "w111" => 0.5
+//, "w112" => 0.5
+//, "w113" => 0.5
+//, "w121" => 0.5
+//, "w122" => 0.5
+//, "w123" => 0.5
+//, "w211" => 0.5
+//, "w212" => 0.5
+//);
+
+if ($do == 'init') {
+    $numPrimer = 1;
+    $inputData = array();
+
+    if (REAL_DATA) {
+        //TODO: сделать реальные данные
+    } else {
+        $sumx = 0;
+        $countExamples = 100;
+
+        for ($ii = 1; $ii <= $countExamples; $ii++) {
+            $xs[] = $ii;
+
+            $sumx += pow($ii, 2);
+        }
+        $sumx = sqrt($sumx);
+
+        foreach ($xs as $i => $x) {
+            $xs[$i] = $x / $sumx;
+        }
+
+        $windowSize = $_POST['windowSize'];
+        for ($i = 0; $i < count($xs) - $windowSize; $i++) {
+            $inputData[] = array(array_slice($xs, $i, $windowSize), array($xs[$i + $windowSize]));
+        }
+
+        foreach ($inputData as $i => $example) {
+            startExample($weights, $example[0], $example[1]);
+        }
+
+        $testData = array(array_slice($xs, count($xs) - $windowSize), ($countExamples + 1) / $sumx, $countExamples + 1, -1);
+
+        $futureTestData = array();
+        for($jj = 0; $jj < 20; $jj++) {
+            $futureTestData[$jj] = $countExamples + $jj + 2;
+        }
+    }
+
+    $er = calcNetworkError($answers, $ethalons);
+
+    $res['error'] = $er;
+    $res['weights'] = $weights;
+    $res['dopWeights'] = $dopWeights;
+    $res['inputData'] = $inputData;
+    $res['answers'] = $answers;
+    $res['eths'] = $ethalons;
+    $res['kfNorm'] = $sumx;
+    $res['testData'] = $testData;
+    $res['futureTestData'] = $futureTestData;
+    echo json_encode($res);
+
+} elseif ($do == 'teach') {
+    $isTeach = true;
+
+    $weights = $_POST['weights'];
+    $dopWeights = $_POST['dopWeights'];
+
+    $countEpochs = $_POST['countEpochs'];
+    $counterEpochs = $_POST['counterEpochs'];
+
+    $teachKoef = $_POST['teachKoef'];
+    $teachKoefStandart = $_POST['teachKoefStandart'];
+    $momentsKoef = $_POST['momentsKoef'];
+
+    $weightsPast = $_POST['weightsPast'];
+    $inputData = $_POST['inputData'];
+
+    for ($j = 0; $j < $countEpochs; $j++) {
+        $numPrimer = 1;
+
+        foreach ($inputData as $i => $example) {
+            startExample($weights, $example[0], $example[1]);
+        }
+
+        if ($_POST['dynamicTeachKoef'])
+            recalcTeachKoef($j);
+    }
+
+    $isTeach = false;
+
+    $answers = array();
+    $ethalons = array();
+
+    $numPrimer = 1;
+    foreach ($inputData as $i => $example) {
+        startExample($weights, $example[0], $example[1]);
+    }
+
+    $er = calcNetworkError($answers, $ethalons);
+
+    $res['error'] = $er;
+    $res['weights'] = $weights;
+    $res['dopWeights'] = $dopWeights;
+    $res['weightsPast'] = $weightsPast;
+    $res['teachKoef'] = $teachKoef;
+    $res['momentsKoef'] = $momentsKoef;
+    echo json_encode($res);
+
+} elseif ($do == 'test') {
+    $weights = $_POST['weights'];
+    $dopWeights = $_POST['dopWeights'];
+
+    $in1 = $_POST['in1'];
+
+    $ans = startExample($weights, $in1);
+
+    $res['in'] = $in1;
+    $res['answer'] = $ans;
+    $res['widths'] = $weights;
+    $res['dopWidth'] = $dopWeights;
+    echo json_encode($res);
+} elseif ($do == 'testing') {
+    $weights = $_POST['weights'];
+    $dopWeights = $_POST['dopWeights'];
+
+    $countTests = $_POST['countTests'];
+    $kfNorm = $_POST['kfNorm'];
+    $testData = $_POST['testData'];
+    $futureTestData = $_POST['futureTestData'];
+
+    $ansAr = array();
+    $want = array();
+
+    $skoSum = 0;
+    for ($ii = 0; $ii < $countTests; $ii++) {
+        $want[] = +$testData[2];
+        $ans = startExample($weights, $testData[0]);
+        $skoSum += pow($testData[1] - $ans[0], 2);
+
+        $testData[0] = array_slice($testData[0], 1);
+        $testData[0][2] = $ans[0];
+        $testData[2] = $futureTestData[$testData[3] + 1];
+        $testData[3] = $testData[3] + 1;
+        $testData[1] = $testData[2] / $kfNorm;
+
+        $ansAr[] = $ans[0] * $kfNorm;
+    }
+
+    $res['testData'] = $testData;
+    $res['ans'] = $ansAr;
+    $res['want'] = $want;
+    echo json_encode($res);
+}
+
+function startExample(&$weights, $x, $eth = false)
+{
+    global $isTeach, $answers, $ethalons, $numPrimer, $hideLayCount, $weightsPast, $momentsKoef;
+    generateX($x);
+
+    $yOut = firstForward($weights, count($x));
+
+    $answers[] = $yOut;
+    $ethalons[] = $eth;
+
+    if (!$isTeach || !$eth) {
+        return $yOut;
+    }
+
+//    foreach ($yOut as $k => $value) {
+    $d = $yOut[0] - $eth[0];
+
+    $ds["d" . ($hideLayCount + 1) . "1"] = $d * calcProizvod($yOut[0]);
+//    }
+
+
+    if ($isTeach) {
+        calcBackPropErrors($ds, $weights);
+
+        $weightsPastTemp = $weights;
+
+        $newWeights = weightCorrection($weights, $x, $ds, $momentsKoef);
+
+        $weightsPast = $weightsPastTemp;
+
+        $weights = $newWeights;
+
+        dopWeightsCorrection($ds);
+    }
+    $numPrimer++;
+}
+
+function generateX($x)
+{
+    for ($i = 0; $i < count($x); $i++) {
+        setValueFuncAct(0, $i + 1, $x[$i]);
+    }
+}
+
+function firstForward(&$weights, $xCount)
+{
+    global $hideLayCount, $hideLayNeuronCount, $outLayNeuronCount, $dopWeights;
+    for ($i = 1; $i <= $hideLayCount + 1; $i++) {
+        $jMax = $i == $hideLayCount + 1 ? $outLayNeuronCount : $hideLayNeuronCount;
+        for ($j = 1; $j <= $jMax; $j++) {
+
+            $kMax = $i == 1 ? $xCount : $hideLayNeuronCount;
+
+            $smtr = 0;
+            for ($k = 1; $k <= $kMax; $k++) {
+                $weightKey = "w" . $i . $j . $k;
+
+                if (!key_exists($weightKey, $weights)) {
+                    $weights[$weightKey] = getRandWeight();
+                }
+                $smtr += getValueFuncAct($i - 1, $k) * $weights[$weightKey];
+            }
+
+            if (!key_exists($j, $dopWeights[$i])) {
+//                $dopWeights[$i][$j] = getRandWeight();
+                $dopWeights[$i][$j] = 0.5;
+            }
+//            $smtr += $dopWeights[$i][$j];
+
+            setValueFuncAct($i, $j, calcFuncActivation($smtr));
+
+            if ($i == $hideLayCount + 1)
+                $y[] = calcFuncActivation($smtr);
+        }
+    }
+
+    return $y;
+}
+
+function calcBackPropErrors(&$ds, $weights)
+{
+    global $hideLayCount, $hideLayNeuronCount, $outLayNeuronCount;
+    for ($i = $hideLayCount; $i > 0; $i--) {
+        for ($j = 1; $j <= $hideLayNeuronCount; $j++) {
+            $kMax = $i == $hideLayCount ? $outLayNeuronCount : $hideLayNeuronCount;
+
+            $curD = 0;
+            for ($k = 1; $k <= $kMax; $k++) {
+                $d = $ds["d" . ($i + 1) . $k];
+                $w = $weights["w" . ($i + 1) . $k . $j];
+
+                $curD += $d * $w;
+            }
+
+            global $numPrimer, $zhop;
+
+            $zhop[$numPrimer]["curD"] = $curD;
+            $zhop[$numPrimer]["Fa"] = getValueFuncAct($i, $j);
+            $zhop[$numPrimer]["proizvod"] = calcProizvod(getValueFuncAct($i, $j));
+
+            $curD *= calcProizvod(getValueFuncAct($i, $j));
+            $zhop[$numPrimer]["curD*proizvod"] = $curD;
+            $ds["d" . $i . $j] = $curD;
+        }
+    }
+}
+
+function weightCorrection($weights, $x, $ds, $momentsKoef)
+{
+    global $teachKoef, $hideLayCount, $hideLayNeuronCount, $outLayNeuronCount, $weightsPast, $zhop2;
+    $newWeights = array();
+
+    for ($i = 1; $i <= $hideLayCount + 1; $i++) {
+        $jMax = $i == $hideLayCount + 1 ? $outLayNeuronCount : $hideLayNeuronCount;
+        for ($j = 1; $j <= $jMax; $j++) {
+
+            $multDsA = $ds["d" . $i . $j] * $teachKoef;
+
+            $zhop2[$i . $j] = $multDsA;
+
+            $kMax = $i == 1 ? count($x) : $hideLayNeuronCount;
+
+            for ($k = 1; $k <= $kMax; $k++) {
+                $newW = getValueFuncAct($i - 1, $k) * $multDsA;
+
+                $wKey = "w" . $i . $j . $k;
+                $w = $weights[$wKey];
+
+                $newW = $w - $newW;
+
+                $momentsKoef *= 1.0;
+                if ($momentsKoef > 0) {
+                    $newW += $momentsKoef * ($w - $weightsPast[$wKey]);
+                }
+
+                $newWeights[$wKey] = $newW;
+            }
+        }
+    }
+    return $newWeights;
+}
+
+function dopWeightsCorrection($ds)
+{
+    global $dopWeights, $teachKoef;
+
+    $faBy1 = calcFuncActivation(1);
+    foreach ($dopWeights as $layNum => $layWeight) {
+        foreach ($layWeight as $neurNum => $dopWeight)
+            if ($layNum == 1)
+                $dopWeights[$layNum][$neurNum] = $dopWeight - $ds["d" . $layNum . $neurNum] * $teachKoef;
+            else
+                $dopWeights[$layNum][$neurNum] = $dopWeight - $ds["d" . $layNum . $neurNum] * $teachKoef * $faBy1;
+    }
+}
+
+
+function getRandWeight()
+{
+    return mt_rand(0, mt_getrandmax() - 1) / mt_getrandmax() - 0.5;
+}
+
+function calcFuncActivation($x)
+{
+    return 1 / (1 + exp(-$x));
+}
+
+function calcProizvod($x)
+{
+    return $x * (1 - $x);
+}
+
+function calcNetworkError($answers, $ethalons)
+{
+    $sum = 0;
+    foreach ($answers as $k => $answerVector) {
+        $ethalonVector = $ethalons[$k];
+        foreach ($answerVector as $l => $answer) {
+            $sum += pow($answer - $ethalonVector[$l], 2);
+        }
+    }
+
+    return sqrt($sum / (count($answers) - 1));
+}
+
+function getValueFuncAct($i, $j)
+{
+    global $funcsAct, $numPrimer;
+    return $funcsAct["f" . $i . $j . "-" . $numPrimer];
+}
+
+function setValueFuncAct($i, $j, $value)
+{
+    global $funcsAct, $numPrimer;
+    $funcsAct["f" . $i . $j . "-" . $numPrimer] = $value;
+}
+
+//function goEpoch(&$weights, $numEpochOnIter = false)
+//{
+//    if ($numEpochOnIter == -1) {
+//
+//        $inputData = array();
+//
+//        $sumx = 0;
+//        for ($ii = 1; $ii <= 100; $ii++) {
+//            $x = 0 + $ii;
+//            $xs[] = $x;
+//
+//            $sumx += pow($x, 2);
+//        }
+//        $sumx = sqrt($sumx);
+//        global $kfNorm;
+//        $kfNorm = $sumx;
+//
+//        foreach ($xs as $i => $x) {
+//            $xs[$i] = $x / $sumx;
+//        }
+//
+//        for ($i = 0; $i < count($xs) - 5; $i++) {
+//            $inputData[] = array(array_slice($xs, $i, 5), array($xs[$i + 5]));
+//        }
+//    } else
+//        $inputData = $_POST['inputData'];
+//
+//    foreach ($inputData as $i => $example) {
+//        startExample($weights, $example[0], $example[1]);
+//    }
+//
+////    if ($numEpochOnIter > 0 && $_POST['dynamicTeachKoef'])
+////        recalcTeachKoef($numEpochOnIter);
+//
+//
+//    return $inputData;
+//}
+
+function recalcTeachKoef($numEpochOnIter)
+{
+    global $teachKoef, $teachKoefStandart;
+
+    $counterEpochs = $_POST['counterEpochs'] + $numEpochOnIter;
+    $teachKoef = $teachKoefStandart / (1 + $counterEpochs / $_POST['startedStopCountStandart']);
+}
+
+function pre($var, $die = false)
+{
+    echo '<pre>';
+    print_r($var);
+    echo '</pre>';
+    if ($die)
+        die('Debug in PRE');
+}
---
+++ neuron/prognosis/script.js
@@ -0,0 +1,286 @@
+var currentWeightsValues;
+var pastWeightsValues;
+var currentDopWeightsValues;
+var counterIteration = 0;
+var counterEpochs = 0;
+var hideLayNeuronCount = 0;
+var windowSize = 0;
+var teachKoefStandart = 0;
+var inputData;
+var testData;
+var futureTestData;
+
+var kfNorm = 0;
+
+var startedStopCountStandart = 0;
+
+var errorsStorage = [['Итерация', 'Значение ошибки'], [0, 0]];
+
+$(function () {
+    $("#init").click(function () {
+        startedStopCountStandart = +$("#maxCountEpochs").val();
+
+        $("#teach").attr("disabled", true);
+        $("#test").attr("disabled", true);
+        $("#testing").attr("disabled", true);
+        counterIteration = 0;
+        counterEpochs = 0;
+        errorsStorage = [['Итерация', 'Значение ошибки']];
+
+        hideLayNeuronCount = $("#hideLayNeuronCount").val();
+        windowSize = $("#windowSize").val();
+        if (teachKoefStandart !== 0)
+            $("#teachKoef").val(teachKoefStandart);
+        teachKoefStandart = $("#teachKoef").val();
+
+        $.ajax({
+            url: "neuro.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                do: 'init',
+                hideLayNeuronCount: hideLayNeuronCount,
+                windowSize: windowSize
+            },
+            success: function (result) {
+                $("#errorsTable td").remove();
+                $("#errorsTable tbody tr").remove();
+                $("#resultTable tbody tr").remove();
+                $(".answer pre").text("");
+
+                let er = result['error'];
+
+                let weights = result['weights'];
+
+                currentWeightsValues = weights;
+                pastWeightsValues = weights;
+                currentDopWeightsValues = result['dopWeights'];
+                inputData = result['inputData'];
+                testData = result['testData'];
+                futureTestData = result['futureTestData'];
+                kfNorm = result['kfNorm'];
+                // console.log(inputData.length);
+                // console.log(testData.length);
+
+                console.log(result);
+                let weightsStr = arrayToStr(weights);
+                $("#weights").text("Текущее значение весов: \n\n" + weightsStr);
+
+                $("#errorsTable tbody").html("<tr><td>" + counterIteration + "</td><td>" + counterEpochs + "</td><td>1</td><td>–</td><td>" + er + "</td></tr>");
+
+                errorsStorage.push([counterIteration, er]);
+                drawChart();
+
+                $("#teach").attr("disabled", false);
+                $("#test").attr("disabled", false);
+                $("#testing").attr("disabled", false);
+            }
+        });
+    });
+
+    function arrayToStr(arr) {
+        let blkstr = [];
+        $.each(arr, function (index, value) {
+            blkstr.push(index + ": " + value);
+        });
+        return blkstr.join("\n");
+    }
+
+    $("#teach").click(function () {
+        toggleDisabled(true);
+
+        counterIteration++;
+
+        let countEpochs = +$("#countEpochs").val();
+        let teachKoef = +$("#teachKoef").val();
+        let dynamicTeachKoef = ($("#dynamicTeachKoef").is(':checked')) ? 1 : 0;
+        let momentsKoef = ($("#useMoments").is(':checked')) ? +$("#momentsKoef").val() : 0;
+
+        // console.log(momentsKoef);
+        $.ajax({
+            url: "neuro.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                do: 'teach',
+                weights: currentWeightsValues,
+                dopWeights: currentDopWeightsValues,
+                countEpochs: countEpochs,
+                teachKoef: teachKoef,
+                teachKoefStandart: teachKoefStandart,
+                dynamicTeachKoef: dynamicTeachKoef,
+                hideLayNeuronCount: hideLayNeuronCount,
+                counterEpochs: counterEpochs,
+                weightsPast: pastWeightsValues,
+                momentsKoef: momentsKoef,
+                startedStopCountStandart: startedStopCountStandart,
+                inputData: inputData,
+            },
+            success: function (result) {
+                counterEpochs += +countEpochs;
+
+                console.log(result);
+                let er = result['error'];
+                // console.log("Ошибка сети: " + er);
+
+                let weights = result['weights'];
+                currentWeightsValues = weights;
+                pastWeightsValues = result['weightsPast'];
+
+                let weightsStr = arrayToStr(weights);
+                $("#weights").text("Текущее значение весов: \n\n" + weightsStr);
+
+                $("#errorsTable tbody tr:first").before("<tr><td>" + counterIteration + "</td><td>" + counterEpochs + "</td><td>" + countEpochs + "</td><td>" + teachKoef + "</td><td>" + er + "</td></tr>");
+
+                errorsStorage.push([counterEpochs, er]);
+                drawChart();
+
+                toggleDisabled(false);
+
+                if (dynamicTeachKoef === 1)
+                    $("#teachKoef").val(result['teachKoef']);
+
+
+                let maxEpochs = $("#maxCountEpochs").val();
+                let stopErrorParam = $("#stopErrorParam").val();
+
+                if (er > stopErrorParam && counterEpochs < maxEpochs && !($("#stopTeach").is(':checked'))) {
+                    $("#test").click();
+                    $("#teach").click();
+                } else {
+                    if (countEpochs >= maxEpochs) {
+                        $("#maxCountEpochs").val(+$("#maxCountEpochs").val() + startedStopCountStandart);
+                    }
+                    $("#test").click();
+                    //$("#stopTeach").prop('checked', false);
+                }
+            }
+        });
+    });
+
+    $("#test").click(function () {
+        let inputTest = $("#inputTest").val();
+        let inputAr = inputTest.split(',');
+
+        for (let i = 0; i < inputAr.length; i++) {
+            inputAr[i] = + (inputAr[i] / kfNorm);
+        }
+
+        console.log(inputAr);
+
+        $("#teach").attr("disabled", true);
+        $("#test").attr("disabled", true);
+        $("#testing").attr("disabled", true);
+
+        $.ajax({
+            url: "neuro.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                do: 'test',
+                weights: currentWeightsValues,
+                dopWeights: currentDopWeightsValues,
+                hideLayNeuronCount: hideLayNeuronCount,
+                in1: inputAr,
+            },
+            success: function (result) {
+                console.log(result);
+
+                $(".answer pre").text("Ответ сети: \n\n" + result['answer'][0] * kfNorm);
+                $("#teach").attr("disabled", false);
+                $("#test").attr("disabled", false);
+                $("#testing").attr("disabled", false);
+            }
+        });
+    });
+
+    $("#testing").click(function () {
+        let countTests = $("#inputTesting").val();
+        // let inputAr = inputTest.split(',');
+        $(".answer2 pre").text("");
+        $("#teach").attr("disabled", true);
+        $("#test").attr("disabled", true);
+        $("#testing").attr("disabled", true);
+
+        $.ajax({
+            url: "neuro.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                do: 'testing',
+                weights: currentWeightsValues,
+                dopWeights: currentDopWeightsValues,
+                hideLayNeuronCount: hideLayNeuronCount,
+                testData: testData,
+                futureTestData: futureTestData,
+                countTests: countTests,
+                kfNorm: kfNorm
+            },
+            success: function (result) {
+                console.log(result);
+                // console.log(testData);
+                // testData = result['testData'];
+                // futureTestData = result['futureTestData'];
+                // console.log(testData)
+
+                // $(".answer2 pre").text("Процент правильных ответов: \n\n" + (result['answer'] * 100) + "\n\n" + result['answerText']);
+
+                $("#resultTable tbody").html("<tr><td>" + result['want'][0] + "</td><td>" + result['ans'][0] + "</td></tr>");
+                for (let i = 1; i < result['want'].length; i++) {
+                    $("#resultTable tbody tr:last").after("<tr><td>" + result['want'][i] + "</td><td>" + result['ans'][i] + "</td></tr>");
+                }
+
+
+                $("#teach").attr("disabled", false);
+                $("#test").attr("disabled", false);
+                $("#testing").attr("disabled", false);
+            }
+        });
+    });
+
+    google.charts.load('current', {packages: ['corechart', 'line']})
+
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        let hideIterat = $("#hideIterat").val();
+        let showStorage = errorsStorage;
+        if (hideIterat < errorsStorage.length - 1) {
+            let a = showStorage.slice(0, 1);
+            let b = showStorage.slice(-(showStorage.length - hideIterat - 1));
+            showStorage = a.concat(b);
+        }
+        var data = google.visualization.arrayToDataTable(showStorage);
+
+        var options = {
+            title: 'График ошибки',
+            hAxis: {
+                title: 'Номер итерации'
+            },
+            vAxis: {
+                title: 'Значение ошибки'
+            },
+            // backgroundColor: '#f1f8e9',
+            backgroundColor: '#ffffff',
+            legend: {position: 'top'}
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+    }
+
+    $("#hideIterat").on("change paste keyup", function () {
+        drawChart();
+    });
+
+    function toggleDisabled(toog) {
+        $("#init").attr("disabled", toog);
+        $("#teach").attr("disabled", toog);
+        $("#test").attr("disabled", toog);
+        $("#testing").attr("disabled", toog);
+        // $("#teachKoef").attr("disabled", toog);
+        $("#useMoments").attr("disabled", toog);
+        $("#momentsKoef").attr("disabled", toog);
+    }
+});
